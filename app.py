9b42675f27544b9da0755e7e90da4566970300e9
ogal7 <ogallager@stuy.edu>
2016-11-03 19:47:01 -0400

    doing helper functions

--- app.py
+++ app.py
@@ -15,27 +15,55 @@ ramirez = Flask(__name__)
 ramirez.secret_key = "fjrjgh??0vjirun??f449929hnf"
 
 
+## Looks into the story entries DB for story IDs where the user matches the current user logged in
+## Returns a list of story IDs that the user is able to read, because they have contributed to them
+
 def storiesIcanView():
     ggg = "SELECT storyId FROM story_entries where user == " + str(session['user']) + ";"
     rara = og.execute(ggg)
     stories = []#list of story IDs you're allowed to choose from
     i = 0
     for item in rara:
-        stories.append(item[i])
-        i+=1
+        if (not(item[0] in stories)):
+            stories.append(item[0])
     return stories
 
-
+## Takes a list of (int) story ids, searches in the story directory for the stories you can view, because you have contributed to them
+## returns a form full of buttons, where each button has the text "title, by user" , is named storytoread, and is matched with it's 
+## story ID
 def buttonifyLinks(storyID):
-    string = "<form action = '/view/'> <b>" 
-    command = "SELECT Title, user from story_directory where storyId == " + str(storyID)+ ""
+    Str = "<form action = '/view/'>" 
+    command = "SELECT Title, user, storyId FROM story_directory WHERE storyId == " + str(storyID)+ ""
     poe = og.execute(command)
-    f = ""
     for item in poe:
-        f+= item[0]
-        f+= item[1]
-    string +=  "<b>Title, by User</b> <input type="text" name=str(storyID)><br><br>"
-    string += " </form>  "
+        f = ""
+        f+= item[0] + " , by "
+        f+= item[1] + 
+        Str += "<button type = 'submit' name = 'storytoread' value = " + str(item[2]) + ">  " + f + "</button>"
+        Str+= "<br>"
+    Str += " </form>  "
+
+
+##Returns a form full of buttons to the first 10 stories in the story directory database
+def stories2Add2():
+    Str = "form action= '/add/'>"
+    command = "SELECT Title, user, storyID FROM story_directory"
+    markSloan = og.execute(command)
+    num = 0
+    for manz in markSloan:
+        if (num < 10):
+            f = ""
+            f += manz[0] + " , by "
+            f += manz[1]
+            Str +=  "<button type = 'submit' name = 'storytoaddto' value = " + str(manz[2]) + ">" + f +"</button>"
+            Str += "<br>"
+            num +=1
+        else:
+            break
+    Str+= "</form>"
+
+
+
 
 
 #hashing passwords
@@ -113,11 +141,13 @@ def main():
         x = " "
 
 
-    lis = storiesIcanView
+    lis = storiesIcanView()
+    LINKS_View = buttonifyLinks(lis) ## All the stories u can view
+
+    LINKS_Add = stories2Add2()
 
 
-
-    return render_template("main.html", user = session["user"], storiesToView= LINKS , storiestoAddto=LINKS, message = x) ## both are lists of links
+    return render_template("main.html", user = session["user"], storiesToView= LINKS_V , storiestoAddto=LINKS_A, message = x) ## both are lists of links
     # message is either "entry successfully added" or "story successfully created"
 
 
