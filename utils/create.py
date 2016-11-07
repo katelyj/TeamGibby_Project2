a4c78fd02e6481742ae3c1ee4860913a8aad749b
Brian-Lu <brianlu22@yahoo.com>
2016-11-06 20:33:52 -0500

    MaxStoryID works

--- utils/create.py
+++ utils/create.py
@@ -2,9 +2,16 @@ from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3
 from datetime import datetime
 
-f = "data/database.db"
-db = sqlite3.connect(f)
-q = db.cursor()
 
 print datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+def findNextStoryID():
+    f = "../data/database.db"
+    db = sqlite3.connect(f)
+    og = db.cursor()
+    command = "SELECT MAX(storyID) FROM story_directory"
+    maxStoryID = og.execute(command)
+    for item in maxStoryID:
+        return item[0]
+    
+findNextStoryID()
